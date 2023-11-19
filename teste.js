5b758c0b02544b774755b9575b76f74e8af42904
Lucas Mendes <111598752+lucasmdcv@users.noreply.github.com>
2023-11-18 22:58:47 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,407 @@
+var botao = document.getElementById('btn',);
+botao.addEventListener('click', function(){
+   
+    alert('você clicou no botao!')
+   
+    alert('Alerta 1!')
+   
+    alert('Alerta 2!')
+   
+    alert('Alerta 3!')
+   
+   
+    alert('Alerta 4!')
+   
+    alert('Alerta 5!')
+   
+    alert('quase lá!')
+   
+    alert('você Errou')
+  
+    alert('Vai desistir ? ')
+
+
+   // ok meu target_blank não funcionou, tenho que avhar outra maneira.
+
+    
+  
+   window.location.href = "https://www.youtube.com/watch?v=tXeI6r9C4HU "
+  
+   window.location.href = "https://www.youtube.com/watch?v=tXeI6r9C4HU "
+  
+   window.location.href = "https://www.youtube.com/watch?v=tXeI6r9C4HU "
+  
+   window.location.href = "https://www.youtube.com/watch?v=tXeI6r9C4HU "
+});
+var botao = document.getElementById('btn1',);
+botao.addEventListener('click', function(){
+
+    alert('você clicou no botao!')
+
+    alert('Alerta 1!')
+
+    alert('Alerta 2!')
+
+    alert('Alerta 3!')
+
+    alert('Alerta 4!')
+
+    alert('Alerta 5!')
+
+    alert('quase lá!')
+
+    alert('você ERROU')
+
+    alert('Vai desistir ? ')
+    
+    window.location.href = "https://www.youtube.com/watch?v=b5xY-jhr3cI "
+
+    window.location.href = "https://www.youtube.com/watch?v=b5xY-jhr3cI "
+
+    window.location.href = "https://www.youtube.com/watch?v=b5xY-jhr3cI "
+
+    window.location.href = "https://www.youtube.com/watch?v=b5xY-jhr3cI "
+    
+});
+var botao = document.getElementById('btn2',);
+botao.addEventListener('click', function(){
+
+    alert('você clicou no botao!')
+
+    alert('Alerta 1!')
+
+    alert('Alerta 2!')
+
+    alert('Alerta 3!')
+
+    alert('Alerta 4!')
+
+    alert('Alerta 5!')
+
+    alert('quase lá!')
+
+    alert('Vai desistir ? ')
+
+
+    window.location.href = "https://www.youtube.com/watch?v=Rb7q3fzgawk "
+
+    window.location.href = "https://www.youtube.com/watch?v=Rb7q3fzgawk "
+
+    window.location.href = "https://www.youtube.com/watch?v=Rb7q3fzgawk "
+
+    window.location.href = "https://www.youtube.com/watch?v=Rb7q3fzgawk "
+    
+});
+var botao = document.getElementById('btn3',);
+    botao.addEventListener('click', function(){
+
+        alert('você clicou no botao!')
+
+        alert('Alerta 1!')
+
+        alert('Alerta 2!')
+
+        alert('Alerta 3!')
+
+        alert('Alerta 4!')
+
+        alert('Alerta 5!')
+
+        alert('quase lá!')
+
+        alert('você ERROU')
+
+        alert('Vai desistir ? ')
+        
+        
+
+        window.location.href = "https://www.youtube.com/watch?v=ZPTHbwcXsJA "
+
+        window.location.href = "https://www.youtube.com/watch?v=ZPTHbwcXsJA "
+
+        window.location.href = "https://www.youtube.com/watch?v=ZPTHbwcXsJA "
+
+        window.location.href = "https://www.youtube.com/watch?v=ZPTHbwcXsJA "
+        
+    });
+    var botao = document.getElementById('btn4',);
+    botao.addEventListener('click', function(){
+
+        alert('você clicou no botao!')
+
+        alert('Alerta 1!')
+
+        alert('Alerta 2!')
+
+        alert('Alerta 3!')
+
+        alert('Alerta 4!')
+
+        alert('Alerta 5!')
+
+        alert('Esse é o correto')
+
+        alert('🤝')
+
+        alert('P-a-r-a-b-é-n-s')
+
+        alert('Esse é o correto')
+
+        alert('P-a-r-a-b-é-n-s')
+
+        alert('Esse é o correto')
+
+        alert('P-a-r-a-b-é-n-s')
+
+        alert('Esse é o correto')
+
+        alert('🤝')
+
+        alert('P-a-r-a-b-é-n-s')
+
+        alert('Esse é o correto')
+
+        alert('P-a-r-a-b-é-n-s')
+
+        alert('Esse é o correto')
+
+        alert('quase lá!')
+
+        alert('você Acertou')
+
+        alert('muito obrigado por jogar !')
+
+        alert('🤝')
+
+        
+        window.location.href = "https://www.youtube.com/watch?v=235k_X8Smrs&t=160s"
+        
+        window.location.href = "https://www.youtube.com/watch?v=235k_X8Smrs&t=160s"
+        
+        window.location.href = "https://www.youtube.com/watch?v=235k_X8Smrs&t=160s"
+        
+        window.location.href = "https://www.youtube.com/watch?v=235k_X8Smrs&t=160s"
+    
+        
+    });
+    var botao = document.getElementById('btn5',);
+    botao.addEventListener('click', function(){
+        
+        alert('você clicou no botao!')
+        
+        alert('Alerta 1!')
+        
+        alert('Alerta 2!')
+        
+        alert('Alerta 3!')
+        
+        alert('Alerta 4!')
+        
+        alert('Alerta 5!')
+        
+        alert('quase lá!')
+        
+        alert('você ERROU')
+        
+        alert('Vai desistir ? ')
+        
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=-7ai4fD7N98 "
+        
+        window.location.href = "https://www.youtube.com/watch?v=-7ai4fD7N98 "
+        
+        window.location.href = "https://www.youtube.com/watch?v=-7ai4fD7N98 "
+        
+        window.location.href = "https://www.youtube.com/watch?v=-7ai4fD7N98 "
+        
+    });
+    var botao = document.getElementById('btn6',);
+    botao.addEventListener('click', function(){
+       
+        alert('você clicou no botao!')
+       
+        alert('Alerta 1!')
+       
+        alert('Alerta 2!')
+       
+        alert('Alerta 3!')
+       
+        alert('Alerta 4!')
+       
+        alert('Alerta 5!')
+       
+        alert('quase lá!')
+       
+        alert('você ERROU')
+       
+        alert('Vai desistir ? ')
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=s2o4zxtqNZ4 "
+       
+        window.location.href = "https://www.youtube.com/watch?v=s2o4zxtqNZ4 "
+       
+        window.location.href = "https://www.youtube.com/watch?v=s2o4zxtqNZ4 "
+       
+        window.location.href = "https://www.youtube.com/watch?v=s2o4zxtqNZ4 "
+        
+    });
+    var botao = document.getElementById('btn7',);
+    botao.addEventListener('click', function(){
+       
+        alert('você clicou no botao!')
+       
+        alert('Alerta 1!')
+       
+        alert('Alerta 2!')
+       
+        alert('Alerta 3!')
+       
+        alert('Alerta 4!')
+       
+        alert('Alerta 5!')
+       
+        alert('quase lá!')
+       
+        alert('você ERROU')
+       
+        alert('Vai desistir ? ')
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+        
+    });
+    var botao = document.getElementById('btn8',);
+    botao.addEventListener('click', function(){
+       
+        alert('você clicou no botao!')
+       
+        alert('Alerta 1!')
+       
+        alert('Alerta 2!')
+       
+        alert('Alerta 3!')
+       
+        alert('Alerta 4!')
+       
+        alert('Alerta 5!')
+       
+        alert('quase lá!')
+       
+        alert('você ERROU')
+       
+        alert('Vai desistir ? ')
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=JCffm9pgw8Y "
+       
+        window.location.href = "https://www.youtube.com/watch?v=JCffm9pgw8Y "
+       
+        window.location.href = "https://www.youtube.com/watch?v=JCffm9pgw8Y "
+       
+        window.location.href = "https://www.youtube.com/watch?v=JCffm9pgw8Y "
+        
+    });
+    var botao = document.getElementById('btn9',);
+    botao.addEventListener('click', function(){
+       
+        alert('você clicou no botao!')
+       
+        alert('Alerta 1!')
+       
+        alert('Alerta 2!')
+       
+        alert('Alerta 3!')
+       
+        alert('Alerta 4!')
+       
+        alert('Alerta 5!')
+       
+        alert('quase lá!')
+       
+        alert('você ERROU')
+       
+        alert('Vai desistir ? ')
+
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+       
+        
+    });
+    var botao = document.getElementById('btn10',);
+    botao.addEventListener('click', function(){
+        
+        alert('você clicou no botao!')
+        
+        alert('Alerta 1!')
+        
+        alert('Alerta 2!')
+        
+        alert('Alerta 3!')
+        
+        alert('Alerta 4!')
+        
+        alert('Alerta 5!')
+        
+        alert('quase lá!')
+        
+        alert('você ERROU')
+        
+        alert('Vai desistir ? ')
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=B_XG6hUFGno"
+        
+        window.location.href = "https://www.youtube.com/watch?v=B_XG6hUFGno"
+        
+        window.location.href = "https://www.youtube.com/watch?v=B_XG6hUFGno"
+        
+        window.location.href = "https://www.youtube.com/watch?v=B_XG6hUFGno"
+        
+    });
+    var botao = document.getElementById('btn11',);
+    botao.addEventListener('click', function(){
+        
+        alert('você clicou no botao!')
+        
+        alert('Alerta 1!')
+        
+        alert('Alerta 2!')
+        
+        alert('Alerta 3!')
+        
+        alert('Alerta 4!')
+        
+        alert('Alerta 5!')
+        
+        alert('quase lá!')
+        
+        alert('você ERROU')
+    
+        alert('Vai desistir ? ')
+        
+        
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+    
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+    
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+    
+        window.location.href = "https://www.youtube.com/watch?v=8cr0YfLL-rk "
+        
+    });
+    
